58e0ebce1f21378e3cf410f107f4fa3340bd9474
maciub1 <92275332+maciub1@users.noreply.github.com>
2023-01-21 17:18:25 +0100

    changed OtherPage.js

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      Multifib
+      Maciej Banaszak
+      GR01
       <Link to="/">Go back home</Link>
     </div>
   );
